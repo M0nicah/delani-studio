83def762348ad053a49f569cb1d34cd2f47ad3bd
M0nicah <monicawughanga@gmail.com>
2022-01-02 21:25:53 +0300

    add hover effect for development

--- js/main.js
+++ js/main.js
@@ -3,7 +3,11 @@ $("#development-description").hide();
 $("#product-management-description").hide();
 //the following functions will show the descriptions on hover(what we do section)
 $("#design").hover(() => {
-    $("#design").replaceWith(("#design-description"));
+    $("#design").replaceWith($("#design-description"));
     $("#design-description").show();
+});
+$("#development").hover(() => {
+    $("#development").replaceWith($("#development-description"));
+    $("#development-description").show();
 });
 
