6c7db9f446511ccd7d7c611f21fc864d7608f6b8
M0nicah <monicawughanga@gmail.com>
2022-01-02 21:20:32 +0300

    add hover effect for design

--- js/main.js
+++ js/main.js
@@ -1,3 +1,9 @@
 $("#design-description").hide();
 $("#development-description").hide();
 $("#product-management-description").hide();
+//the following functions will show the descriptions on hover(what we do section)
+$("#design").hover(() => {
+    $("#design").replaceWith(("#design-description"));
+    $("#design-description").show();
+});
+
