e9295b19b1804c2d1427abfd6fbbdc326b2edc59
M0nicah <monicawughanga@gmail.com>
2022-01-12 16:06:22 +0300

    fix 1: toggle functions

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ $("#design-description").hide();
 $("#development-description").hide();
 $("#product-management-description").hide();
 //the following functions will show the descriptions on hover(what we do section)
-$("#design").toggle(() => {
+/*$("#design").toggle(() => {
     $("#design").replaceWith($("#design-description"));
     $("#design-description").show();
 });
@@ -19,6 +19,27 @@ document.getElementById("btn").addEventListener("click",() => {
     const name = document.getElementById("inputName").value();
     const email = document.getElementById("inputEmail").value();
     const message = document.getElementById("inputMessage").value();
-});
+});*/
 
-if (inputName ==
+$(document).ready(function(){
+    $("#design").click (function(){
+        $("#design-description").toggle();
+        $("#design").show();
+    });
+
+}); 
+$(document).ready(function(){
+    $("#development").click (function(){
+        $("#development-description").toggle();
+        $("#development").show();
+    });
+
+}); 
+$(document).ready(function(){
+    $("#product-management").click (function(){
+        $("#product-management-description").toggle();
+        $("#product-management").show();
+    });
+
+}); 
+
